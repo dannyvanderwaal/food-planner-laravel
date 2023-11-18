842ac5152709d191577bfb5c3a9210e88b85a21a
Danny van der Waal <danny.vanderwaal@notive.nl>
2023-11-18 16:34:44 +0100

    feat: Removed dashboard page to make the home page the dashboard page

--- routes/web.php
+++ routes/web.php
@@ -21,10 +21,6 @@ Route::get('/', [HomeController::class, 'index'])->middleware(['auth', 'verified
 //    Route::get('/', [HomeController::class, 'index'])->name('home');
 //});
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
